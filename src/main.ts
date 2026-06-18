81c76975ef381d52e4b85c1dfa648dc74ed43815
imransoftvence <imranmia@softvence.com>
2026-06-18 10:29:29 +0600

    done payment

--- src/main.ts
+++ src/main.ts
@@ -54,7 +54,7 @@ async function bootstrap() {
       ...configuredOrigins,
       'http://localhost:5173',
       'http://localhost:5174',
-      'http://localhost:5500',
+      'https://onset-depletion-vanity.ngrok-free.dev',
       'http://127.0.0.1:5500',
       'http://localhost:5501',
       'http://127.0.0.1:5501',
